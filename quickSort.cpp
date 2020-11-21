f98d82885a9fb3d877475a98d5fbfc77dc233313
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-11-21 23:43:23 +0900

    Add quickSort algorithm file

--- quickSort.cpp
+++ quickSort.cpp
@@ -11,6 +11,51 @@
 // j가 i보다 작아지면 j번째와 첫번째 인덱스(피봇)를 교환
 // 그럼 피봇보다 작은 숫자들이 피봇 왼쪽, 큰 숫자들은 오른쪽으로 붙는다.
 // 계속반복
+
+void Swap(int* a, int* b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void _Sort(int list[], int left, int right) {
+	if (left <= right) {
+		int pivot = left;
+		int i = left + 1;
+		int j = right;
+
+		do {
+			while (list[pivot] > list[i])
+				++i;
+			while (list[pivot] < list[j])
+				--j;
+
+			if (i <= j) {
+				Swap(&list[i], &list[j]);
+				++i;
+				--j;
+			}
+		} while (i <= j);
+		Swap(&list[pivot], &
+			list[j]);
+		_Sort(list, left, j - 1);
+		_Sort(list, j+1, right);
+	}
+
+}
+
+void Sort(int list[], int size) {
+	_Sort(list, 0, size - 1);
+}
+
 int main() {
 	int list[10] = { 50, 20, 70, 80, 60, 90, 40, 30, 45, 75 };
+
+	Sort(list, 10);
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%5d",list[i]);
+	}
+
 }
